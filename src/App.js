03b98a6f8261e1cddf6987e65f38e9019121cf5f
deepto <ddepp98@outlook.com>
2023-01-20 12:40:33 +0530

    Added ambient light

--- src/App.js
+++ src/App.js
@@ -31,10 +31,13 @@ class App extends Component {
 
     scene.add(cube);
 
-    //Add Light
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.6)
-    scene.add(ambientLight)
+    //Add Lights
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+    scene.add(ambientLight);
 
+    const directionalLight =new THREE.DirectionalLight(0xffffff,0.6);
+    directionalLight.position.set(10,20,0);
+    scene.add(directionalLight);
 
     camera.position.z = 5;
 
