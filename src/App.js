353b813988c52af113f27ead5bb22726b9d401eb
deepto <ddepp98@outlook.com>
2023-01-20 22:08:09 +0530

    Abstracted scene creation into an init function

--- src/App.js
+++ src/App.js
@@ -2,65 +2,61 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
 
-const App = () => {
+var scene, camera, renderer;
 
+const App = () => {
   const mountRef = useRef(null);
 
   useEffect(() => {
-    //Create Scene
-    var scene = new THREE.Scene();
-
-    //Adding Cube
-    var geometry = new THREE.BoxGeometry(3, 1, 3);
-    var material = new THREE.MeshLambertMaterial({ color: 0xfb8e00 });
-    var cube = new THREE.Mesh(geometry, material);
-
-    scene.add(cube);
-
-    //Add Lights
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-    scene.add(ambientLight);
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
-    directionalLight.position.set(10, 20, 0);
-    scene.add(directionalLight);
-
-    //Add Camera
-
-    //Perspective Camera
-    // var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 100);
-    // // camera.position.set(4, 4, 4);
-    // camera.position.z = 5;
-    // camera.lookAt(0, 0, 0);
-
-    //Orthographic Camera
-    const width = 10;
-    const height = width * (window.innerHeight / window.innerWidth);
-    var camera = new THREE.OrthographicCamera(width / -2, width / 2, height / 2, height / -2, 1, 100);
-    camera.position.set(4, 4, 4);
-    camera.lookAt(0, 0, 0);
-
-    //Setup Renderer
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.render(scene, camera);
-
-    mountRef.current.appendChild(renderer.domElement);
-
-    //Resize on window size change
-    let onWindowResize = function () {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-    }
-    window.addEventListener("resize", onWindowResize, false);
-
+    init(mountRef)
     return () => mountRef.current.removeChild(renderer.domElement);
-  }, []);
+  });
 
   return (
     <div ref={mountRef} />
   );
+
 }
 
+//This function handles creating the scene
+const init = (mountRef) => {
+  //Create Scene
+  scene = new THREE.Scene();
+
+  //Adding Cube
+  var geometry = new THREE.BoxGeometry(3, 1, 3);
+  var material = new THREE.MeshLambertMaterial({ color: 0xfb8e00 });
+  var cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  //Add Lights
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+  directionalLight.position.set(10, 20, 0);
+  scene.add(directionalLight);
+
+  //Add Orthographic Camera
+  const width = 10;
+  const height = width * (window.innerHeight / window.innerWidth);
+  camera = new THREE.OrthographicCamera(width / -2, width / 2, height / 2, height / -2, 1, 100);
+  camera.position.set(4, 4, 4);
+  camera.lookAt(0, 0, 0);
+
+  //Setup Renderer
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+  mountRef.current.appendChild(renderer.domElement);
+
+  //Resize on window size change
+  let onWindowResize = function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+  window.addEventListener("resize", onWindowResize, false);
+
+}
 export default App;
